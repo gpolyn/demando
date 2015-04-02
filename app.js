bacae84aa0160bd18a0105968effe025758a5a94
Gallagher Polyn <gallagher.polyn@gmail.com>
2015-04-02 07:58:26 -0400

    succesful photo-sharing and persistence with twilio + also installed all required packages

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 // Require dependencies
+var db = require('mongoskin').db('mongodb://192.168.59.103:27017/exampleDb'); 
 var http = require('http');
 var path = require('path');
 var express = require('express');
 var twilio = require('twilio');
 var bodyParser = require('body-parser');
+require('dotenv').load();
  
+var smss = db.collection('smss');
+
 // Create Express app and HTTP server, and configure socket.io
 var app = express();
 var server = http.createServer(app);
@@ -19,10 +23,10 @@ app.use(bodyParser.urlencoded({
 app.use(express.static(path.join(__dirname, 'static')));
  
 // Configure port for HTTP server
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5050);
  
 // Handle incoming MMS messages from Twilio
-app.post('/message', function(request, response) {
+app.post('/twilio', function(request, response) {
     console.log('Received message.');
     var twiml = twilio.TwimlResponse();
     var numMedia = parseInt(request.body.NumMedia);
@@ -30,7 +34,14 @@ app.post('/message', function(request, response) {
     if (numMedia > 0) {
         for (i = 0; i < numMedia; i++) {
             var mediaUrl = request.body['MediaUrl' + i];
+            var messageSid = request.body['MessageSid'];
+            var from = request.body['From'];
+            var body = request.body['Body'];
             console.log('Displaying MediaUrl: ' + mediaUrl);
+            smss.insert({mediaUrl: mediaUrl, messageSid: messageSid, from: from, body: body}, function (err, result) {
+                console.log("persisted in mongo!");
+                console.log(result);
+            });
             io.emit('newMedia', mediaUrl);
         }
         twiml.message('Photo received - check the screen to see it pop up!');
